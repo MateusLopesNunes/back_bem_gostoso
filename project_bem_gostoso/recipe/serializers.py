a430299e91d0022e4c9aa93a17be97c2e776b9a1
mateus lopes <mateuslopesnunes@outlook.com>
2023-05-29 19:44:46 -0300

    depth

--- project_bem_gostoso/recipe/serializers.py
+++ project_bem_gostoso/recipe/serializers.py
@@ -6,11 +6,13 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+        depth = 5
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id','title', 'number_of_portion', 'preparation_method', 'preparation_time', 'created_at', 'updated_at', 'recipe_image', 'category', 'user', 'ingredients']
+        depth = 5
 
 class RecipeCreateSerializer(serializers.ModelSerializer):
     #ingredient = serializers.ListField(child=serializers.CharField(max_length=100))
--- project_bem_gostoso/user/serializers.py
+++ project_bem_gostoso/user/serializers.py
@@ -6,6 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'birth_date', 'telephone', 'email', 'password', 'created_at', 'updated_at']
+        depth = 5
 
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
